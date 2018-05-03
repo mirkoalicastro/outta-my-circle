112d0a44b1e2becb5e065ffdf9c165c552696b1d
Mirko Alicastro <mirkoalicastro@gmail.com>
2018-05-03 10:48:35 +0200

    AI-3.1 <Mirko@Mirko Overwrite remote https://github.com/mirkoalicastro/outta-my-circle.git to local

--- app/src/main/java/com/badlogic/androidgames/framework/Audio.java
+++
@@ -1,7 +0,0 @@
-package com.badlogic.androidgames.framework;
-
-public interface Audio {
-    public Music newMusic(String filename);
-
-    public Sound newSound(String filename);
-}
--- app/src/main/java/com/badlogic/androidgames/framework/Sound.java
+++
@@ -1,7 +0,0 @@
-package com.badlogic.androidgames.framework;
-
-public interface Sound {
-    public void play(float volume);
-
-    public void dispose();
-}
