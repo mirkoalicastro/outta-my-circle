6f086762f87ed6b9820c2180a073e39bf5ff51e4
daniel_san <danielecuomo@github.com>
2018-05-15 18:30:46 +0200

    Try

--- app/src/main/java/com/example/mfaella/physicsapp/entitycomponent/impl/PowerUp.java
+++ app/src/main/java/com/example/mfaella/physicsapp/entitycomponent/impl/PowerUp.java
@@ -3,5 +3,6 @@ package com.example.mfaella.physicsapp.entitycomponent.impl;
 public abstract class PowerUp extends GameObject {
     //protected Character character = null;
 
-    public abstract void attack();
+    public abstract void modify();
+    public abstract void restore();
 }
