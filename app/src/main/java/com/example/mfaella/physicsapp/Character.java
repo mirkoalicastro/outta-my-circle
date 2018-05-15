0309817b9c486bba3615366c8268043a1e73c1f9
daniel_san <danielecuomo@github.com>
2018-05-15 14:34:40 +0200

    Try

--- app/src/main/java/com/example/mfaella/physicsapp/Character.java
+++ app/src/main/java/com/example/mfaella/physicsapp/Character.java
@@ -1,9 +1,18 @@
 package com.example.mfaella.physicsapp;
 
+import com.example.mfaella.physicsapp.entitycomponent.PositionComponent;
+import com.google.fpl.liquidfun.World;
+
 public class Character extends GameObject{
     PowerUp powerUp = null;
 
-    public void move(){
+    public static Character defaultFactory(World world, float radius, float x, float y){
+        Character c = new Character();
 
+        c.addComponent(new PositionComponent(x, y));
+        c.addComponent(new CirclePhysicsComponent(world, radius));
+        c.addComponent(new CircleDrawableComponent());
+
+        return c;
     }
 }
--- app/src/main/java/com/example/mfaella/physicsapp/entitycomponent/PositionComponent.java
+++ app/src/main/java/com/example/mfaella/physicsapp/entitycomponent/PositionComponent.java
@@ -3,6 +3,11 @@ package com.example.mfaella.physicsapp.entitycomponent;
 public class PositionComponent extends Component {
     public float x, y;
 
+    public PositionComponent(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
     public Component.Type type() {
         return Component.Type.Position;
