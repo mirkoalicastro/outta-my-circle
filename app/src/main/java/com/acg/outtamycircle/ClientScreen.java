d84160bd14e023c5a2f8fc37a74795b115910702
Mirko <mirkoalicastro@gmail.com>
2018-09-11 16:42:02 +0200

    Updated client server screen

--- app/src/main/java/com/acg/outtamycircle/ClientScreen.java
+++ app/src/main/java/com/acg/outtamycircle/ClientScreen.java
@@ -1,5 +1,9 @@
 package com.acg.outtamycircle;
 
+import android.util.Log;
+
+import com.acg.outtamycircle.entitycomponent.Component;
+import com.acg.outtamycircle.entitycomponent.DrawableComponent;
 import com.acg.outtamycircle.entitycomponent.impl.GameCharacter;
 import com.acg.outtamycircle.network.GameMessage;
 import com.acg.outtamycircle.network.googleimpl.ClientMessageReceiver;
@@ -7,10 +11,13 @@ import com.acg.outtamycircle.network.googleimpl.MyGoogleRoom;
 import com.badlogic.androidgames.framework.impl.AndroidGame;
 
 public class ClientScreen extends ClientServerScreen {
-    public ClientScreen(AndroidGame game, MyGoogleRoom myGoogleRoom, String[] players, byte[] skins, int[][] spawnPositions) {
-        super(game, myGoogleRoom, players, skins, spawnPositions);
+    private boolean isAlive = true;
+    public ClientScreen(AndroidGame game, MyGoogleRoom myGoogleRoom, String[] players, byte[] skins, int[][] spawnPositions, int playerOffset) {
+        super(game, myGoogleRoom, players, skins, spawnPositions, playerOffset);
 
-        initCharacterSettings(40);
+        int radiusCharacter = 40;
+
+        initCharacterSettings(radiusCharacter);
 
         GameCharacter[] characters = new GameCharacter[players.length];
         for (int i = 0; i < characters.length; i++)
@@ -22,11 +29,46 @@ public class ClientScreen extends ClientServerScreen {
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
-        GameCharacter ch;
+
+        if(isAlive) {
+            GameMessage message = GameMessage.createInstance();
+            interpreter.makeMoveClientMessage(message, (short)playerOffset, androidJoystick.getNormX(), androidJoystick.getNormY());
+            myGoogleRoom.getNetworkMessageHandlerImpl().putInBuffer(message);
+            if(shouldAttack) {
+                shouldAttack = false;
+                interpreter.makeAttackMessage(message, (short)playerOffset);
+                myGoogleRoom.getNetworkMessageHandlerImpl().putInBuffer(message);
+            }
+            myGoogleRoom.getNetworkMessageHandlerImpl().sendUnreliable(myGoogleRoom.getServerId());
+            GameMessage.deleteInstance(message);
+        }
 
         for (GameMessage message : myGoogleRoom.getNetworkMessageHandlerImpl().getMessages()) {
             switch (interpreter.getType(message)){
-                case MOVE_CLIENT:
+                case MOVE_SERVER:
+                    int objectId = interpreter.getObjectId(message);
+                    float posX = interpreter.getPosX(message);
+                    float posY = interpreter.getPosY(message);
+                    float rotation = interpreter.getRotation(message);
+                    DrawableComponent comp = (DrawableComponent) status.characters[objectId].getComponent(Component.Type.Drawable);
+                    comp.setX((int)posX).setY((int)posY);
+                    break;
+                case ATTACK:
+                    //TODO
+                    break;
+                case DESTROY:
+                    //TODO se muoio setta vivo a false
+                    //TODO
+                    break;
+                case POWERUP:
+                    //TODO piazza powerup da qualche parte
+                    break;
+                case POWERUP_ASSIGN:
+                    //TODO devo gestirlo?
+                    break;
+                case END:
+                    int winnerId = interpreter.getObjectId(message);
+                    Log.d("WINNERIS", winnerId + " ha vinto");
                     break;
             }
         }
